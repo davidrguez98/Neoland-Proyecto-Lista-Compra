0974b0666543e9e72ace701f8ad404788a05a41c
davidrguez98 <ropeda98@gmail.com>
2025-05-01 13:16:30 +0200

    acceso a web de administrador

---
+++ strings/functions.js
@@ -0,0 +1,26 @@
+/* ============ OBJETO ============ */
+
+class Article {
+
+    constructor(id, img, name, description, quantity) {
+        this.id = id
+        this.img  = img
+        this.name = name
+        this.description = description
+        this.quantity = quantity
+    }
+}
+
+/* ============ FUNCIONES ============ */
+
+function addArticle() {
+
+}
+
+function editArticle() {
+
+}
+
+function deleteArticle() {
+    
+}
