560e6c991c9b92553ca202d92ae2bcb6b60530f5
davidrguez98 <ropeda98@gmail.com>
2025-05-02 11:55:45 +0200

    pagina de añadir y editar articulo hechas

--- strings/functions.js
+++ strings/functions.js
@@ -8,12 +8,22 @@ class Article {
         this.name = name
         this.description = description
         this.quantity = quantity
+        this.size = [250, 500, 1.000]
     }
 }
 
 /* ============ FUNCIONES ============ */
 
-function addArticle() {
+let counter = 1
+
+function addArticle(img, name, description = "No description", quantity) {
+
+    id = counter
+    counter++
+
+    let newArticle = new Article(id, img, name, description, 3)
+
+    return newArticle
 
 }
 
@@ -24,3 +34,11 @@ function editArticle() {
 function deleteArticle() {
     
 }
+
+let coffee1 = addArticle("foto1", "Colombia", "Café suave", 2)
+let coffee2 = addArticle("foto1", "Guatemala", "Café suave", 2)
+let coffee3 = addArticle("foto1", "Peru", "Café suave", 2)
+
+console.log(coffee1)
+console.log(coffee2)
+console.log(coffee3)
