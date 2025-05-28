18eea4899d72caac42bd61f16d532198720d01d9
davidrguez98 <ropeda98@gmail.com>
2025-05-28 16:04:48 +0200

    guardado de avance de boton edit

--- scripts/localStorage/localStorage-scripts.js
+++ scripts/localStorage/localStorage-scripts.js
@@ -36,3 +36,62 @@ export function deleteItemLocalStorage() {
         })
     }
 }
+
+export function editItemLocalStorage() {
+
+    const selectedButton = document.querySelectorAll("#StorageList #editButton")
+
+    for (let index = 0; index < selectedButton.length; index++) {
+        selectedButton[index].addEventListener("click", function() {
+            console.log("boton" + index)
+            // window.location.assign("./update-article.html")
+
+            // document.querySelector(".editArticleContainer").innerHTML += `
+            // <div class="mt-4 w-75 d-flex justify-content-center">
+            //         <form id="myForm" class="w-100 p-3 rounded-3 shadow-sm" style="background-color: var(--color-background);">  
+            //             <div class="mb-2">
+            //                 <input class="form-control img-js" type="file" id="formFile">
+            //             </div>
+
+            //             <div class="input-group mb-2">
+            //                 <span class="input-group-text" id="basic-addon1">Product's name</span>
+            //                 <input type="text" class="form-control" id="name-js" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+            //             </div>
+
+            //             <div class="input-group mb-2">
+            //                 <span class="input-group-text">Description</span>
+            //                 <textarea class="form-control" id="description-js" aria-label="With textarea"></textarea>
+            //             </div>
+
+            //             <div class="input-group mb-2">
+            //                 <span for="quantity" class="input-group-text">Quantity</span>
+            //                 <input type="number" class="form-control quantity-js" id="quantity" value="1" min="1" style="width: 80px;">
+            //             </div>
+
+            //             <div class="input-group mb-2">
+            //                 <span class="input-group-text">Size</span>
+            //                 <select class="form-select py-0 mb-0 size-js" id="floatingSelect" aria-label="Floating label select example">
+            //                     <option selected value="250">250 gr</option>
+            //                     <option value="500">500 gr</option>
+            //                     <option value="1000">1 Kg</option>
+            //                 </select>
+            //             </div>
+
+            //             <div class="input-group mb-2">
+            //                 <span for="price" class="input-group-text">Price</span>
+            //                 <input type="number" class="form-control quantity-js" id="price" style="width: 80px;">
+            //             </div>
+
+            //             <div class="d-flex justify-content-between gap-2">
+            //                 <a class="btn" style="background-color: var(--color-buttons); color: var(--color-background);" href="./admin-web.html" role="button">Cancel</a>
+            //                 <div>
+            //                     <input class="btn btn-primary border-0 new-article-js" id="new-article-js" type="submit" value="Add article" style="background-color: var(--color-buttons);">
+            //                     <input class="btn btn-primary border-0" type="reset" value="Reset article" style="background-color: var(--color-buttons);">
+            //                 </div>
+            //             </div>
+            //         </form>
+            // </div>
+            // `
+        })
+    }
+}
--- scripts/pages/admin-web-scripts.js
+++ scripts/pages/admin-web-scripts.js
@@ -1,14 +1,17 @@
 import { displayLocalStorage } from "../object-functions.js"
-import { deleteItemLocalStorage, resetLocalStorage } from "../localStorage/localStorage-scripts.js"
+import { deleteItemLocalStorage, resetLocalStorage, editItemLocalStorage } from "../localStorage/localStorage-scripts.js"
 
 export function mainAdminWeb() {
+
     displayLocalStorage()
     deleteItemLocalStorage()
-
+    editItemLocalStorage()
+    
     document.querySelector(".reset-storage-js").addEventListener("click", function (event) {
         event.preventDefault()
         resetLocalStorage()
         displayLocalStorage()
     })
+    
 }
 
